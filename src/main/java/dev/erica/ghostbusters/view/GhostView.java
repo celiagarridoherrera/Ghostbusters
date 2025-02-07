41c8f8f4bf2c884721cf2e58d7f68db57b5264ab
Celia Garrido <celiagarridoherrera@gmail.com>
2025-02-07 14:55:37 +0100

    implement List of Ghosts view

--- src/main/java/dev/erica/ghostbusters/view/GhostView.java
+++ src/main/java/dev/erica/ghostbusters/view/GhostView.java
@@ -1,5 +1,22 @@
 package dev.erica.ghostbusters.view;
 
+import java.util.List;
+
+import dev.erica.ghostbusters.model.GhostModel;
+
 public class GhostView {
 
+    public void showGhosts(List<GhostModel> ghosts) {
+        if (ghosts.isEmpty()) {
+            System.out.println("No hay fantasmas capturados");
+        } else {
+            System.out.println("\nFantasmas capturados");
+            System.out.println("====================================================");
+            System.out.println("ID   Nombre                          Clase                Peligrosidad  Fecha");
+            System.out.println("------------------------------------------------------------------------------");
+            ghosts.forEach(System.out::println);
+            System.out.println("------------------------------------------------------------------------------");
+            System.out.println("Total: " + ghosts.size());
+        }
+    }
 }
