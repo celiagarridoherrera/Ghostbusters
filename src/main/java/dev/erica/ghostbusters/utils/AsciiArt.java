12d1aa9b8140b607e6925fb59928ff1c3ebf3987
Erica <you@example.com>
2025-02-09 20:06:54 +0100

    create main menu, text archives and method for ascii art and ansi colors

---
+++ src/main/java/dev/erica/ghostbusters/utils/AsciiArt.java
@@ -0,0 +1,19 @@
+package dev.erica.ghostbusters.utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class AsciiArt {
+    public static void printAsciiArt(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                line = line.replace("\\033", "\033");
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error al cargar el arte ASCII: " + e.getMessage());
+        }
+    }
+}
